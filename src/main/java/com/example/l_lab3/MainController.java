8d11f48f6d981b3d5dc01815a1a1dc17fa4b2d90
Anton <voevodin_anton_2000@mail.ru>
2021-12-22 02:03:09 +0400

    add session

--- src/main/java/com/example/l_lab3/MainController.java
+++ src/main/java/com/example/l_lab3/MainController.java
@@ -5,19 +5,83 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.File;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String getIndex(Model model){
-        model.addAttribute("operation", new SumOperation());
+    @GetMapping
+    public String getIndex(HttpServletRequest httpServletRequest) {
+        SumOperation sumOperation = new SumOperation();
+
+        httpServletRequest.getSession().setAttribute("MY_SESSION_NUMBERS", sumOperation);
+
         return "index";
     }
 
-    @PostMapping("/")
-    public String doOperation(@ModelAttribute SumOperation sumOperation, Model model){
-        model.addAttribute("result", sumOperation.getA() + sumOperation.getB());
+    @PostMapping
+    public String doOperation(Model model,
+                              @RequestParam("fnumber") Double fNumber,
+                              @RequestParam("lnumber") Double lNumber,
+                              HttpServletRequest httpServletRequest,
+                              HttpSession httpSession) {
+
+        SumOperation sumOperation = (SumOperation) httpSession.getAttribute("MY_SESSION_NUMBERS");
+
+        if (sumOperation == null) {
+            try {
+                Thread.sleep(5000);
+                model.addAttribute("errorMessage", "Ошибка сложения чисел");
+                return "redirect:/";
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        sumOperation.setA(fNumber);
+        sumOperation.setB(lNumber);
+
+        httpServletRequest.getSession().setAttribute("MY_SESSION_NUMBERS", sumOperation);
+
+        model.addAttribute("sumOperation", sumOperation);
         return "result";
+    }
+
+    @PostMapping("/xml")
+    public String writeToXml(Model model, HttpSession httpSession) {
+        SumOperation sumOperation = (SumOperation) httpSession.getAttribute("MY_SESSION_NUMBERS");
+
+        writeToXml(sumOperation);
+
+        model.addAttribute("sumOperation", sumOperation);
+        model.addAttribute("successMessage", "Успешно выгружено в xml");
+        return "result";
+    }
+
+    private void writeToXml(SumOperation sumOperation) {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(SumOperation.class);
+
+            //Create Marshaller
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+            //Required formatting??
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+            //Store XML to File
+            File file = new File("sumOperation.xml");
+
+            //Writes XML file to file-system
+            jaxbMarshaller.marshal(sumOperation, file);
+        } catch (JAXBException ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/com/example/l_lab3/SumOperation.java
+++ src/main/java/com/example/l_lab3/SumOperation.java
@@ -1,8 +1,15 @@
 package com.example.l_lab3;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "operation")
+@XmlAccessorType(XmlAccessType.PROPERTY)
 public class SumOperation {
     private double a;
     private double b;
+    private double result;
 
     public double getA() {
         return a;
@@ -18,5 +25,13 @@ public class SumOperation {
 
     public void setB(double b) {
         this.b = b;
+    }
+
+    public double getResult(){
+        return a + b;
+    }
+
+    public void setResult(double result){
+        this.result = result;
     }
 }
